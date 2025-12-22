0ee6df98fedf7be368610c9bdb738ddcf77642fe
atillaturan <atilla.turan@edeka.de>
2025-12-22 11:44:04 +0100

    feat: adjust all Entities' Search Operations to check additional attributes

--- models/forms/CrmFilter.php
+++ models/forms/CrmFilter.php
@@ -32,18 +32,29 @@ class CrmFilter extends Model
         if (!empty($this->term)) {
             switch ($entityType) {
                 case 'organization':
-                    $query->andWhere(['like', 'name', $this->term]);
-                    $query->orWhere(['like', 'city', $this->term]);
+                    $query->andWhere(['like', 'crm_organization.name', $this->term])
+                        ->orWhere(['like', 'crm_organization.category', $this->term])
+                        ->orWhere(['like', 'crm_organization.industry', $this->term])
+                        ->orWhere(['like', 'crm_organization.city', $this->term])
+                        ->orWhere(['like', 'crm_organization.notes', $this->term]);
                     break;
                 case 'contact':
-                    $query->andWhere(['like', 'name', $this->term])
-                        ->orWhere(['like', 'email', $this->term]);
+                    $query->andWhere(['like', 'crm_contact.name', $this->term])
+                        ->orWhere(['like', 'crm_contact.email', $this->term])
+                        ->orWhere(['like', 'crm_contact.phone_number', $this->term])
+                        ->orWhere(['like', 'crm_contact.gender', $this->term]);
+                    // TODO: Evaluieren: Organisation als Suchbegruff?
                     break;
                 case 'interaction':
-                    $query->andWhere(['like', 'crm_interaction.title', $this->term]);
+                    $query->andWhere(['like', 'crm_interaction.title', $this->term])
+                        ->orWhere(['like', 'crm_interaction.channel', $this->term])
+                        ->orWhere(['like', 'crm_interaction.description', $this->term])
+                        ->orWhere(['like', 'crm_interaction.result', $this->term]);
+                    // TODO: Evalurieren: Möglichkeit nach Firmen/Kontaktpersonen/Resp Users zu filtern?
                     break;
                 case 'event':
-                    $query->andWhere(['like', 'crm_event.title', $this->term]);
+                    $query->andWhere(['like', 'crm_event.title', $this->term])
+                        ->orWhere(['like', 'crm_event.description', $this->term]);
                     break;
             }
         }
@@ -55,7 +66,7 @@ class CrmFilter extends Model
 
         // Interactions
         if ($entityType === 'interaction') {
-            if (in_array(self::FILTER_OVERDUE , $this->filters)) {
+            if (in_array(self::FILTER_OVERDUE, $this->filters)) {
                 $query->andWhere(['<', 'date', date('Y-m-d')])
                     ->andWhere(['=', 'crm_interaction.status', 'OVERDUE']);
             }
