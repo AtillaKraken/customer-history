2f6b9a8c67bb7b4e4295796fac90ea53bb75afe1
atillaturan <atilla.turan@edeka.de>
2025-12-23 13:56:01 +0100

    feat: remove VisibilityLink from the entities' ControlsMenu and add custom EditLink using the custom edit-modal

--- widgets/WallEntry.php
+++ widgets/WallEntry.php
@@ -4,6 +4,8 @@ namespace app\modules\crm\widgets;
 
 // humhubs class to display stream entries
 use humhub\modules\content\widgets\stream\WallStreamEntryWidget;
+use humhub\modules\content\widgets\EditLink;
+use humhub\modules\content\widgets\VisibilityLink;
 
 class WallEntry extends WallStreamEntryWidget
 {
@@ -12,7 +14,45 @@ class WallEntry extends WallStreamEntryWidget
      */
     protected function renderContent()
     {
+        return InteractionCard::widget([
+            'interaction' => $this->model,
+            'isStream' => true
+        ]);
+    }
 
-        return InteractionCard::widget(['interaction' => $this->model]);
+    public function getControlsMenuEntries()
+    {
+        $entries = parent::getControlsMenuEntries();
+
+        foreach ($entries as $key => $entry) {
+            if (is_array($entry)) {
+                $class = $entry[0] ?? null;
+
+                // remove Visibility Link from menu
+                if ($class === VisibilityLink::class) {
+                    unset($entries[$key]);
+                }
+
+                // remove existing EditLink (to use the custome one instead => using the edit-modal)
+                if ($class === EditLink::class) {
+                    unset($entries[$key]);
+                }
+            }
+        }
+
+        // custom Edit-Link using the modal
+        if ($this->model->content->canEdit()) {
+            $entries[] = [
+                EditLink::class,
+                [
+                    'model' => $this->model,
+                    'url' => $this->model->content->container->createUrl('/crm/interaction/edit', ['id' => $this->model->id]),
+                    'mode' => self::EDIT_MODE_MODAL
+                ],
+                ['sortOrder' => 100]
+            ];
+        }
+
+        return $entries;
     }
 }
